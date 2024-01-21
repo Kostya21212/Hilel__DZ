40b8e0e2736bafb755f054a351374fdb957e933d
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-01-21 15:44:00 +0200

    Calculate

--- js.js
+++ js.js
@@ -1,20 +1,83 @@
-function addNum() {
-  const firstNum = prompt("Введіть перше число");
- 
- const secondNum = prompt("Введіть друге число");
+let a = ''; // перше число
+let b = ''; // друге число
+let sign = ''; // знак операції
 
- const thirdNum = prompt("Введіть третье число");
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'x', '/'];
 
-  if (firstNum === null || secondNum === null || thirdNum === null) {
-    alert("Ви вийшли");
-  } else if (isNaN(firstNum) || isNaN(secondNum) || isNaN(thirdNum)) {
-    alert("Введіть будь ласка число");
-  } else if (firstNum === "" || secondNum === "" || thirdNum === "") {
-    alert("Нічого не написано!");
-  } else {
-    const result = (Number(firstNum) + Number(secondNum) + Number(thirdNum)) / 3;
-    alert(`Середне значення 3-х чисел : ${result}`);
-  }
+// екран
+const out = document.querySelector('#input-calc-res');
+const outResult = document.querySelector('#input-calc-res2')
+console.log(out);
+
+function clear() {
+    a = '';
+    b = '';
+    sign = '';
+    out.value = '0';
 }
-addNum();
 
+document.querySelector('#red-btn').onclick = clear;
+
+document.querySelector('.wrap').onclick = (event) => {
+    // натиснута не кнопка
+    if (!event.target.classList.contains('btn')) return;
+    // натиснута кнопка clear
+    if (event.target.classList.contains('red-btn')) return;
+
+    // отримую натискану кнопку
+    const key = event.target.textContent;
+
+    // перевіряю, чи це перше чи друге число, і встановлюю значення a або b відповідно
+    if (digit.includes(key)) {
+        if (sign === '') {
+            a += key;
+            out.value = a;
+        } else {
+            b += key;
+            out.value = a + sign + b;
+        }
+    } else if (action.includes(key)) {
+        sign = key;
+        out.value = a + sign;
+        
+    }
+
+    // якщо нажата кнопка + - / *
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        return;
+    }
+
+    // нажата кнопка =
+    if (key === '=') {
+        calculate();
+    }
+};
+
+function calculate() {
+    if (a === '' || b === '' || sign === '') {
+        // перевіряємо, чи є достатньо значень для обчислень
+        return;
+    }
+
+    switch (sign) {
+        case '+':
+            a = (parseFloat(a) + parseFloat(b)).toString();
+            break;
+        case '-':
+            a = (parseFloat(a) - parseFloat(b)).toString();
+            break;
+        case 'x':
+            a = (parseFloat(a) * parseFloat(b)).toString();
+            break;
+        case '/':
+            a = (parseFloat(a) / parseFloat(b)).toString();
+            break;
+    }
+
+    outResult.value = a;
+    b = ''; // очистимо значення b для нового введення
+    sign = ''; // очистимо знак операції
+}
